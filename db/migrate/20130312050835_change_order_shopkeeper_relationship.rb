c2bf7715e657d761c8b7e25a7c03657a98e6dbb8
Sarah Edris <sedris@mit.edu>
2013-03-12 13:13:25 -0400

    changed user_id to shopper_id in cart to fix heroku

--- db/migrate/20130312050835_change_order_shopkeeper_relationship.rb
+++ db/migrate/20130312050835_change_order_shopkeeper_relationship.rb
@@ -6,10 +6,12 @@ class ChangeOrderShopkeeperRelationship < ActiveRecord::Migration
 		end
 
 		rename_column :orders, :user_id, :cart_id
+    rename_column :carts, :user_id, :shopper_id
   end
 
   def down
   	drop_table 'orders_shopkeepers'
   	rename_column :orders, :cart_id, :user_id
+    rename_column :carts, :shopper_id, :user_id
   end
 end
