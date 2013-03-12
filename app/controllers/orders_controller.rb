d9de0da9481f9ee68e172802c61f8ad702cb51be
Sarah Edris <sedris@mit.edu>
2013-03-12 10:03:54 -0400

    can view orders by shopper and shopkeeper

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,22 @@
 class OrdersController < ApplicationController
 	def index
 		@orders = Order.all
+		@shopkeeper = Shopkeeper.find_by_id(session[:user_id])
+    # if no shopkeeper was found
+    if !@shopkeeper
+    	# TODO: authenticate
+      @shopkeeper = Shopkeeper.all.last
+    end
+
+		@shopkeeper_orders = []
+		Order.all.each do |o|
+			if o.shopkeepers.include?(@shopkeeper)
+				@shopkeeper_orders.append(o)
+			end
+		end
 	end
 
+	# creates an order from a specified cart
 	def create
 		@cart = current_user.cart
 		@order = current_user.cart.create_order()
@@ -12,7 +26,9 @@ class OrdersController < ApplicationController
 				@order.shopkeepers << item.shopkeeper
 			end
 		end 
-		current_user.cart = Cart.create()
-		redirect_to cart_path :notice => current_user.cart.id
+
+		# empty the cart
+		current_user.cart.items = []
+		redirect_to cart_path
 	end
 end
