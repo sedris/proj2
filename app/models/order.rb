9c728db9f1b0775cb4c9b063c0e9af2de829e7da
Sarah Edris <sedris@mit.edu>
2013-03-17 23:48:36 -0400

    added design

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,23 +1,10 @@
 class Order < ActiveRecord::Base
   attr_accessible :user_id
-  attr_accessor :shopper
-  attr_reader :shopper
   belongs_to :cart
   has_and_belongs_to_many :items
   has_and_belongs_to_many :shopkeepers
 
   validate :cart_not_empty, :before => :create
-
-  # order either came from shopper's main cart or saved cart
-  def shopper()
-    if order.cart.shopper
-      self.shopper = order.cart.shopper
-      @shopper = order.cart.shopper
-    else
-      self.shopper = order.cart.saved.shopper
-      @shopper = order.cart.saved.shopper
-    end 
-  end
 
   private
 	  def cart_not_empty
